c848f4e0e7c531f181af4543cef30db0c6a848ba
Viatcheslav Sukhodoiev <suhodoiev@gmail.com>
2021-02-22 13:25:46 +0200

    task 5 from lesson 2 completed

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -4,7 +4,7 @@
 # Создайте списки:
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-my_family = []
+my_family = ['Mother', 'Father', 'Julia', 'Katia', 'Dima']
 
 
 # список списков приблизителного роста членов вашей семьи
@@ -13,8 +13,16 @@ my_family_height = [
     [],
 ]
 
+my_family_height = [['Father', 170], ['Mother', 155], ['Julia', 168], ['Katia', 160], ['Dima', 104]]
+
+print(my_family_height)
+
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
+print('height my father ', my_family_height[0][1], ' cm')
+
 
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+general_height = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1] + my_family_height[3][1] + my_family_height[4][1]
+print('my family general height ', general_height, ' cm')
