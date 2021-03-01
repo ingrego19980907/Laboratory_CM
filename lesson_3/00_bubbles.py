0cab4237dd202963aed49843f5b7c403cf8c281d
Viatcheslav Sukhodoiev <suhodoiev@gmail.com>
2021-03-01 09:50:04 +0200

    task 1 completed

--- lesson_3/00_bubbles.py
+++ lesson_3/00_bubbles.py
@@ -4,21 +4,32 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
-# Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+def bubble(point, step):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(center_position=point, radius=radius, width=2)
+
+
+# point_1 = sd.get_point(500, 400)
+# bubble(point=point_1, step=15)
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+for x in range(100, 1001, 100):
+    point = sd.get_point(x, 100)
+    bubble(point=point, step=5)
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+for y in range(100, 301, 100):
+    for x in range(100, 1001, 100):
+        point = sd.get_point(x, y)
+        bubble(point=point, step=5)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+for _ in range(100):
+    point = sd.random_point()
+    bubble(point=point, step=5)
 
 sd.pause()
-
-
