405546edfe64bab170993eb44ac2a60f27b96c42
Viatcheslav Sukhodoiev <suhodoiev@gmail.com>
2021-03-01 12:45:58 +0200

    task 3 completed

--- lesson_3/03_division.py
+++ lesson_3/03_division.py
@@ -8,6 +8,9 @@
 # Формат вывода:
 #   Целочисленное деление ХХХ на YYY дает ZZZ
 
-a, b = 179, 37
-
-# TODO здесь ваш код
+a, b = 168, 37
+answer = 0
+while a > b:
+    a -= b
+    answer += 1
+print(answer)
