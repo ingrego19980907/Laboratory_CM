3cb1b55b30bd4b541a4f04b4e3afea776361c95c
Viatcheslav Sukhodoiev <suhodoiev@gmail.com>
2021-04-06 13:59:13 +0300

    MainEntropy and InformationGrowth counted

--- study/examples/SampleEntropyIG.py
+++ study/examples/SampleEntropyIG.py
@@ -9,8 +9,7 @@
 # Licence:     <your licence>
 # -------------------------------------------------------------------------------
 
-import \
-    pandas as pd  # Щоб не використовувати довгу назву pandas, скорочуємо подальше посилання на бібліотеку до двох літер
+import pandas as pd  # Щоб не використовувати довгу назву pandas, скорочуємо подальше посилання на бібліотеку
 import math
 
 
@@ -24,8 +23,8 @@ def stint1(x):
                                      # тому для нього визначається окрема функція
 
 
-xlsx = pd.ExcelFile(
-    r'G:\GEXF\МКДС/3.xlsx')  # вводиться змінна xlsx, яка має значення рядка, що є фізичною адресою файла Excel
+xlsx = pd.ExcelFile(r'C:\Python\Project\Study\Laboratory\Tasks\Task_1\Task_1.xlsx')
+# вводиться змінна xlsx, яка має значення рядка, що є фізичною адресою файла Excel
 """Читаємо окремі стовпчики з файлу за адресою xlsx
 #Параметр usecols='A,B' задає назви стовпчиків, які
 читаються і заносяться у стовпчики DataFrame df
@@ -42,16 +41,15 @@ xlsx = pd.ExcelFile(
 namescol = ['PE', 'Diameter']
 df = pd.read_excel(xlsx, usecols='A,B', header=None, names=namescol, nrows=50,
                    converters={'PE': stint1, 'Diameter': stint})
-print(df)
 
 # сортуємо дані за стовпчиком СС
 sorted_df = df.sort_values(by='PE')
 
 # записують результат сортування у файл Excel за допомогою функції to_excel
-sorted_df.to_excel('G:/GEXF/МКДС/3n.xlsx', header=False, index=False)
+sorted_df.to_excel('C:/Python/Project/Study/Laboratory/Tasks/Task_1/result_1.xlsx', header=False, index=False)
 
 df['CC'] = df['PE'] + df['Diameter']
-df['FFFF'] = 10. * df['CC'] - df['Diameter']
+df['FF'] = 10. * df['CC'] - df['Diameter']
 print(df)
 
 # порахуємо кількість рядків у PE з різними значеннями
@@ -65,7 +63,7 @@ bb = {}
 bb = df['PE'].value_counts() / elem_count
 MainEntropy = -bb[0] * math.log(bb[0]) - bb[1] * math.log(bb[1])
 print("Виводимо словник ймовірностей! \n", bb)
-# Створюємо словник, в якому ключами будуть значення, а елементами кількість об'єктів в кожній групі
+# # Створюємо словник, в якому ключами будуть значення, а елементами кількість об'єктів в кожній групі
 cc = {}
 cc = df['Diameter'].value_counts()
 # Створюємо список ключів словника сс
@@ -76,40 +74,40 @@ print(kl)
 # Кількість груп
 ngroup = len(kl)
 print(ngroup)
-# Створюємо список кількостей елементів у кожній групі
+# # Створюємо список кількостей елементів у кожній групі
 nig = []
 for i in range(ngroup):
     nig.append(cc[kl[i]])
 print('nig=', nig)
-av = 0.;
+av = 0.
 for i in range(len(kl)):
-    av = av + kl[i] * nig[i] / elem_count
+    av += kl[i] * nig[i] / elem_count
 print('average value', av)
 vav = {"Diameter": av}
 VaV = {}
 for col in namescol:
     VaV.update({col: av})
 print(VaV)
-# Створюємо список кількостей одиниць і нулів у кожній групі
-# Зовнішній вимір буде відповідати кількості груп, а внутрішній - дорівнює двом, тобто відповідає двом різним значенням
-# у першому стовпчику
+# # Створюємо список кількостей одиниць і нулів у кожній групі
+# # Зовнішній вимір буде відповідати кількості груп, а внутрішній - дорівнює двом, тобто відповідає двом різним значенням
+# # у першому стовпчику
 sss = []
 for i in range(ngroup):
     sss.append([])
     for j in range(2):
         sss[i].append(0.0)
 print(sss)
-# Рахуємо кількість нулів та одиниць у першому стовпчику для кожної з груп
+# # Рахуємо кількість нулів та одиниць у першому стовпчику для кожної з груп
 for i in range(elem_count):
     for j in range(ngroup):
         if df['Diameter'][i] == kl[j]:  # Вибираємо елемент j-ої групи
             if df['PE'][i] == 0.0:  # Якщо стовпчику PE для цільового атрибута стоїть значення нуль, то
-                sss[j][0] = sss[j][0] + 1.  # рахуємо нулі і результат заносимо на першу позицію j-ої групи
+                sss[j][0] += 1.  # рахуємо нулі і результат заносимо на першу позицію j-ої групи
             else:
-                sss[j][1] = sss[j][1] + 1.
+                sss[j][1] += 1.
                 # У іншому випадку рахуємо одиниці і результат заносимо на другу позицію j-ої групи
 print(sss)
-# Рахуємо ентропії для кожної з груп
+# # Рахуємо ентропії для кожної з груп
 Entropies = []
 for i in range(ngroup):
     EG = 0.
@@ -124,10 +122,10 @@ for i in range(ngroup):
 
 print(Entropies)
 
-# Розрахунок приросту інформації для стовпчика Diameter
+# # Розрахунок приросту інформації для стовпчика Diameter
 IG = MainEntropy
 for i in range(ngroup):
-    IG = IG - Entropies[i] * nig[i] / elem_count
+    IG -= Entropies[i] * nig[i] / elem_count
 
 print('MainEntropy=', MainEntropy)
 print('InformationGrowth=', IG)
