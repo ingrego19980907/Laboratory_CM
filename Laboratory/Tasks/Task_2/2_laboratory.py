da0c4bb6403bbf5cd2b5ff0b516a995b71dfe315
Viatcheslav Sukhodoiev <suhodoiev@gmail.com>
2021-04-29 15:04:14 +0300

    Laboratory 2 completed

--- Laboratory/Tasks/Task_2/2_laboratory.py
+++ Laboratory/Tasks/Task_2/2_laboratory.py
@@ -1,11 +1,9 @@
 from pprint import pprint
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from PerceptronRosenblat_module import Perceptron, plot_decision_regions
-
+from matplotlib.colors import ListedColormap
 
 print(
     '\n\n\n\n\n___Завдання №1 Читати дані з таблиці Excel і заноситиме їх у DataFrame, переводячи їх у числові, '
@@ -193,6 +191,71 @@ print('\n\n\n\n\n___Завдання №5 Будуватиме класифік�
       'Перевірити роботу моделей на решті 25 грибах.')
 
 
+
+class Perceptron(object):
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w_= rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+
+        self.errors_ = []
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for (idx,cl) in enumerate(np.unique(y)):
+        print(type(idx))
+        print(type(cl))
+        print(type((idx,cl)))
+        plt.scatter(X[y == cl,0],
+                    X[y == cl,1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black')
+
+
 QUANT_DATA = 25
 
 df_2 = pd.read_excel(r'C:\Python\Project\Study\Laboratory\Tasks\Task_1\Task_1.xlsx', usecols='A,B,C',
@@ -212,12 +275,10 @@ print('X\n', X)
 # plot data
 
 for i in range(QUANT_DATA):
-    if(y[i] == -1):
-        plt.scatter(X[i, 0], X[i, 1],
-            color='red', marker='o')
+    if y[i] == -1:
+        plt.scatter(X[i, 0], X[i, 1], color='red', marker='o')
     else:
-        plt.scatter(X[i, 0], X[i, 1],
-            color='blue', marker='x')
+        plt.scatter(X[i, 0], X[i, 1], color='blue', marker='x')
 
 
 plt.xlabel('sepal length [cm]')
